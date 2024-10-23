44d3c15bcc6c04ca0d416d99989343d9a85532bc
batirniyaz <muratbaevbatirniyaz@gmail.com>
2024-10-23 23:53:46 +0500

    include swagger, redoc and task routes

--- todo_proweb/urls.py
+++ todo_proweb/urls.py
@@ -1,2 +1,13 @@
 
 from django.contrib import admin
+from django.urls import path, include
+from rest_framework import permissions
+from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, SpectacularSwaggerView
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('task/', include('task.urls')),
+    path('swagger/', SpectacularSwaggerView.as_view(), name='swagger-ui'),
+    path('redoc/', SpectacularRedocView.as_view(), name='redoc'),
+    path('schema/', SpectacularAPIView.as_view(), name='schema'),
+]
