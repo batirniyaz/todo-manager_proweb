51c4c9db8184d10081c938975a9560f094e552f1
batirniyaz <muratbaevbatirniyaz@gmail.com>
2024-10-24 22:34:44 +0500

    add new type of authorization and delete other ones

--- todo_proweb/settings.py
+++ todo_proweb/settings.py
@@ -59,9 +59,7 @@ MIDDLEWARE = [
 REST_FRAMEWORK = {
     'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
     'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework.authentication.SessionAuthentication',
-        'rest_framework.authentication.BasicAuthentication',
-        'rest_framework.authentication.TokenAuthentication',
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
     ],
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticated',
