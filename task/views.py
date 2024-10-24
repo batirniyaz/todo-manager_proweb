2eb153269a7710c2fc494e7b74ddc7063c102eaa
batirniyaz <muratbaevbatirniyaz@gmail.com>
2024-10-25 00:35:45 +0500

    remove unnecessary code

--- task/views.py
+++ task/views.py
@@ -9,12 +9,6 @@ from .serializers import TaskSerializer
 from rest_framework import status
 
 
-class TaskListAPIViewPagination(PageNumberPagination):
-    page_size = 10
-    page_size_query_param = 'page_size'
-    max_page_size = 100
-
-
 class TaskListAPIView(APIView):
 
     @extend_schema(
