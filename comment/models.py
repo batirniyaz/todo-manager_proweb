efeed616b653a1f299a8c09181b3b195a25881ca
batirniyaz <muratbaevbatirniyaz@gmail.com>
2024-10-25 03:04:41 +0500

    create comment model

--- comment/models.py
+++ comment/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Comment(models.Model):
+    text = models.TextField(max_length=255, blank=False, null=False)
+
+    task = models.ForeignKey('task.Task', on_delete=models.CASCADE, related_name='comments')
+    user = models.ForeignKey('auth.User', on_delete=models.CASCADE, related_name='comments')
+
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.text[:20]
