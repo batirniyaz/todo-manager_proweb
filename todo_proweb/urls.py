490284216f8ee3bd4e0e9472b32bd2cadea3acaf
batirniyaz <muratbaevbatirniyaz@gmail.com>
2024-10-24 22:37:59 +0500

    configure root urls file to authorization

--- todo_proweb/urls.py
+++ todo_proweb/urls.py
@@ -3,10 +3,19 @@ from django.contrib import admin
 from django.urls import path, include
 from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, SpectacularSwaggerView
 
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+    TokenVerifyView
+)
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('task/', include('task.urls')),
     path('swagger/', SpectacularSwaggerView.as_view(), name='swagger-ui'),
     path('redoc/', SpectacularRedocView.as_view(), name='redoc'),
     path('schema/', SpectacularAPIView.as_view(), name='schema'),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
 ]
