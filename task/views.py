7d715326d36085736d9005f919a2afc8e8fe997e
batirniyaz <muratbaevbatirniyaz@gmail.com>
2024-10-25 00:35:20 +0500

    remove unnecessary import

--- task/views.py
+++ task/views.py
@@ -1,6 +1,5 @@
 from drf_spectacular.utils import extend_schema, OpenApiExample, OpenApiParameter, OpenApiResponse
 from rest_framework.pagination import PageNumberPagination
-from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
